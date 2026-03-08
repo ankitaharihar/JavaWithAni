bcf195038366f208badb4ec66245735978e34690
Ankita <ankitaharihar81@gmail.com>
2026-03-09 03:27:42 +0530

    Understanding Variable

--- Variables/main.java
+++ Variables/main.java
@@ -1,5 +1,10 @@
 package Variables;
 
 public class main {
-    
+    public static void main(String a[]){
+    int num1 = 7;
+    int num2 = 5;
+    int result = num1 + num2 ;
+    System.out.println(result);
+}
 }
