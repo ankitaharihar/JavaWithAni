c6116d7aaaabda20667848c0e40ca34bf2cf0f08
Ankita <ankitaharihar81@gmail.com>
2026-04-01 16:03:14 +0530

    Remove HelloWorld and main Java files; add Day1 Java program to find missing number

---
+++ Day1.java
@@ -0,0 +1,15 @@
+public class Day1 {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,5,6,7};
+        int n = 7;
+
+        int sum = (n*(n+1))/2;
+        int arrSum = 0;
+        for(int i=0; i<arr.length; i++){
+            arrSum += arr[i];
+        }
+        int missingNum = sum - arrSum;
+        System.out.println("The missing number is: " + missingNum);  
+    
+}
+}
