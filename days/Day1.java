985df6284352617360e54d1639866d4848a989c3
Ankita <ankitaharihar81@gmail.com>
2026-04-02 21:10:17 +0530

    Refactor Day1.java: remove missing number logic and update to chocolate calculation; add new Pattern.java for pattern printing

--- days/Day1.java
+++ days/Day1.java
@@ -1,16 +1,35 @@
+// Missing Number in an Array
+// package days;
+// public class Day1 {
+//     public static void main(String[] args) {
+//         int[] arr={1,2,3,5,6,7};
+//         int n = 7;
+
+//         int sum = (n*(n+1))/2;
+//         int arrSum = 0;
+//         for(int i=0; i<arr.length; i++){
+//             arrSum += arr[i];
+//         }
+//         int missingNum = sum - arrSum;
+//         System.out.println("The missing number is: " + missingNum);  
+    
+// }
+// }
+
+//Free Chocalte 
 package days;
 public class Day1 {
     public static void main(String[] args) {
-        int[] arr={1,2,3,5,6,7};
-        int n = 7;
+        int choco = 50;
+        int wrap = choco;
+        int free = 0;
+        while (wrap>=3) {
+            int newChoco = wrap/3;
+            free = free + newChoco;
+            wrap = newChoco + (wrap % 3);
 
-        int sum = (n*(n+1))/2;
-        int arrSum = 0;
-        for(int i=0; i<arr.length; i++){
-            arrSum += arr[i];
+            
         }
-        int missingNum = sum - arrSum;
-        System.out.println("The missing number is: " + missingNum);  
-    
-}
+        System.out.println("total choco: " + (free));
+    }
 }
---
+++ days/Pattern.java
@@ -0,0 +1,19 @@
+package days;
+public class Pattern {
+    public static void main(String[] args) {
+        int rows = 5;
+        
+        for (int i = 1; i <= rows; i++) {
+            for (int j = i; j <= rows; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print((char)('A' + j));
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("Hi");
+            }
+            System.out.println();
+        }
+    }
+}
