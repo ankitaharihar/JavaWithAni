772218658e7d00dea039b54b38446d046829e576
Ankita <ankitaharihar81@gmail.com>
2026-04-22 10:41:00 +0530

    Added problems

--- Problem set 2/FindNextTerm.java
+++
@@ -1,8 +0,0 @@
-public class FindNextTerm {
-    public static void main(String[] args) {
-        int n = 5;
-        int next = n * n;
-        System.out.println("Next Term oblem hei uske do solution ek hi file mei h" + next);
-    }
-    
-}
