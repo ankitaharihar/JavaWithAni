a25d03f895055be5bb7db07187c98f15af689f3f
Ankita Harihar <137799793+ankitaharihar@users.noreply.github.com>
2026-05-04 20:55:46 +0530

    Fix formatting in MissingNuminSeries.java

--- src/main/java/LogicalProblems/MissingNuminSeries.java
+++ src/main/java/LogicalProblems/MissingNuminSeries.java
@@ -4,7 +4,7 @@ public class MissingNuminSeries {
 public static void main(String[] args) {
 
 	        int[] arr = {1, 2, 3, 5};
-	        int n = 5; // expected range
+	        int n = 5; 
 
 	        int expectedSum = n * (n + 1) / 2;
 	        int actualSum = 0;
