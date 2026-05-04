698be9ed5e714eda202162aa64f9b9da70ca373d
Ankita Harihar <137799793+ankitaharihar@users.noreply.github.com>
2026-05-04 20:58:19 +0530

    Remove comment about changing value of n

--- src/main/java/LogicalProblems/PrintReverseNto1.java
+++ src/main/java/LogicalProblems/PrintReverseNto1.java
@@ -2,7 +2,7 @@ package LogicalProblems;
 
 public class PrintReverseNto1 {
 	public static void main(String[] args) {
-		int n = 10; // Change this value to print from n to 1
+		int n = 10; 
 
 		for (int i = n; i >= 1; i--) {
 			System.out.println(i);
