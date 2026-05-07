ab5efc4f3a02073df02f48769505a6ad7b120110
Ankita Harihar <137799793+ankitaharihar@users.noreply.github.com>
2026-05-07 21:46:26 +0530

    Implement isAnagram method in Logic class
    Added isAnagram method to check if two strings are anagrams.

--- src/main/java/com/ani/app/method/problem25/Logic.java
+++ src/main/java/com/ani/app/method/problem25/Logic.java
@@ -1,5 +1,15 @@
 package com.ani.app.method.problem25;
 
-public class Logic {
+import java.util.Arrays;
 
+public class Logic {
+    public boolean isAnagram(String s1, String s2) {
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+        
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        
+        return Arrays.equals(arr1, arr2);
+    }
 }
