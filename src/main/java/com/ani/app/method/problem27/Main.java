f56bf898e1c9bdb6689bbebdb28247d1c7c9c586
Ankita Harihar <137799793+ankitaharihar@users.noreply.github.com>
2026-05-07 21:45:12 +0530

    Add main method to Main class

--- src/main/java/com/ani/app/method/problem27/Main.java
+++ src/main/java/com/ani/app/method/problem27/Main.java
@@ -1,5 +1,8 @@
 package com.ani.app.method.problem27;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Logic logic = new Logic();
+        logic.countAll("this is java program number 27");
+    }
 }
